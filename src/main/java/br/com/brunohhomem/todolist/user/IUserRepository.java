c2531b5e8952da6c5a6a9cb2295a6c552d8460ce
Bruno H. Homem <brunohhomem@gmail.com>
2023-10-10 12:00:10 -0300

    Aula 02

---
+++ src/main/java/br/com/brunohhomem/todolist/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package br.com.brunohhomem.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+  UserModel findByUsername(String username);
+
+}
